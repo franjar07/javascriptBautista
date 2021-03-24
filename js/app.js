443937e36c7b6e623398498dea229417d9b8e22c
Francisco Bautista <francisco.m1015@gmail.com>
2021-03-24 10:33:39 -0500

    Desafio 2, validaciones

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
-// const actualYear = 2021;
-
-// let myName = prompt("Ingrese su nombre");
-// let mylastName = prompt("Ingrese su apellido");
-// let yearOne = parseInt(prompt("Ingresa tu año de nacimiento"));
-// let oldCity = prompt("Ingrese la ciudad donde nació");
-// let myCity = prompt("Ingrese la ciudad donde vive");
-// let duration = parseInt(prompt("Hace cuantos años vive en esta ciudad"));
-// let age = actualYear - yearOne;
-// let ageOrigin = actualYear - duration;
-
-// alert("Hola " + myName + " " + mylastName + ", tu edad es " + age + " años" + " y vives en " + myCity + " desde el " + ageOrigin + ".")
+const actualYear = 2021;
+let myName = prompt("Ingrese su nombre");
+let yearOne = parseInt(prompt("Ingresa tu año de nacimiento"));
+let age = actualYear - yearOne;
+if (age<18) {
+    alert("Hola " + myName + ", debes ser mayor de edad para ingresar a este sitio")
+} else {
+    let city = prompt("Por favor ingrese su ciudad de residencia");
+    if ((city == "Bogotá") || (city == "Bogota") || (city == "bogotá") || (city == "bogota")) { alert("Bienvenido " + myName + ". Gracias por registrarte")
+    } else {
+        alert("Debe residir en Bogotá para poder participar");
+    }
+}
