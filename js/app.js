a15b0f2de4bd273144a99ffcb13b577b90d60572
Francisco Bautista <francisco.m1015@gmail.com>
2021-03-24 15:44:04 -0500

    sin cambios sustanciales

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@ if (age<18) {
     alert("Hola " + myName + ", debes ser mayor de edad para ingresar a este sitio")
 } else {
     let city = prompt("Por favor ingrese su ciudad de residencia");
-    if ((city == "Bogotá") || (city == "Bogota") || (city == "bogotá") || (city == "bogota")) { alert("Bienvenido " + myName + ". Gracias por registrarte")
+    if ((city == "Bogotá") || (city == "Bogota") || (city == "bogotá") || (city == "bogota")) { 
+    alert("Bienvenido " + myName + ". Gracias por registrarte")
     } else {
         alert("Debe residir en Bogotá para poder participar");
     }
