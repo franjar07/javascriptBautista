5b229aa631934340c9aec90e0200583ca60f3eeb
Francisco Bautista <francisco.m1015@gmail.com>
2021-03-24 09:50:54 -0500

    Entrega del desafio 2: condicionales

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-const actualYear = 2021;
+// const actualYear = 2021;
 
-let myName = prompt("Ingrese su nombre");
-let mylastName = prompt("Ingrese su apellido");
-let yearOne = parseInt(prompt("Ingresa tu año de nacimiento"));
-let oldCity = prompt("Ingrese la ciudad donde nació");
-let myCity = prompt("Ingrese la ciudad donde vive");
-let duration = parseInt(prompt("Hace cuantos años vive en esta ciudad"));
-let age = actualYear - yearOne;
-let ageOrigin = actualYear - duration;
+// let myName = prompt("Ingrese su nombre");
+// let mylastName = prompt("Ingrese su apellido");
+// let yearOne = parseInt(prompt("Ingresa tu año de nacimiento"));
+// let oldCity = prompt("Ingrese la ciudad donde nació");
+// let myCity = prompt("Ingrese la ciudad donde vive");
+// let duration = parseInt(prompt("Hace cuantos años vive en esta ciudad"));
+// let age = actualYear - yearOne;
+// let ageOrigin = actualYear - duration;
 
-alert("Hola " + myName + " " + mylastName + ", tu edad es " + age + " años" + " y vives en " + myCity + " desde el " + ageOrigin + ".")
+// alert("Hola " + myName + " " + mylastName + ", tu edad es " + age + " años" + " y vives en " + myCity + " desde el " + ageOrigin + ".")
